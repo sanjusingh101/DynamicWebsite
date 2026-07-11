269e398eefd845bc7180de373e6a8d0eb56f3618
sanjusingh101 <sanjufromamity@gmail.com>
2026-07-11 15:37:07 +0530

    change redirect target

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-
-// Current page shown to all visitors
-let currentPage = "home.html";
-
-// Home route
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "pages", currentPage));
+
+    const control = JSON.parse(
+        fs.readFileSync("./control.json", "utf8")
+    );
+
+    if (control.mode === "redirect") {
+        return res.redirect(control.redirect);
+    }
+
+    if (control.mode === "page") {
+        return res.sendFile(
+            path.join(__dirname, "pages", control.page)
+        );
+    }
+
+    res.send("Invalid configuration.");
 });
 
-// Admin page
-app.get("/admin", (req, res) => {
-    res.sendFile(path.join(__dirname, "pages", "admin.html"));
-});
-
-// Change page
-app.post("/change", (req, res) => {
-    currentPage = req.body.page;
-    res.redirect("/admin");
-});
-
-// Start server (works locally and on Render)
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Running on port ${PORT}`);
 });
