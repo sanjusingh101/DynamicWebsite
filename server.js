9c424c5786b855a59bc8c36fc774c016e19b7822
sanjusingh101 <sanjufromamity@gmail.com>
2026-07-11 15:41:46 +0530

    Added admin redirect panel

--- server.js
+++ server.js
@@ -1,30 +1,65 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Change this password
+const ADMIN_PASSWORD = "123456";
+
+// Current redirect
+let redirectUrl = "https://www.youtube.com";
+
+// Visitor
 app.get("/", (req, res) => {
+    res.redirect(redirectUrl);
+});
 
-    const control = JSON.parse(
-        fs.readFileSync("./control.json", "utf8")
-    );
+// Admin page
+app.get("/admin", (req, res) => {
+    res.send(`
+        <h2>Website Controller</h2>
 
-    if (control.mode === "redirect") {
-        return res.redirect(control.redirect);
+        <form method="POST" action="/update">
+            Password:<br>
+            <input type="password" name="password"><br><br>
+
+            Redirect URL:<br>
+            <input
+                type="text"
+                name="url"
+                value="${redirectUrl}"
+                style="width:400px"
+            ><br><br>
+
+            <button type="submit">
+                Update Redirect
+            </button>
+        </form>
+
+        <p>Current URL: ${redirectUrl}</p>
+    `);
+});
+
+// Update redirect
+app.post("/update", (req, res) => {
+
+    if (req.body.password !== ADMIN_PASSWORD) {
+        return res.send("Wrong password");
     }
 
-    if (control.mode === "page") {
-        return res.sendFile(
-            path.join(__dirname, "pages", control.page)
-        );
-    }
+    redirectUrl = req.body.url;
 
-    res.send("Invalid configuration.");
+    res.send(`
+        Redirect updated.<br><br>
+
+        <a href="/admin">Back</a>
+    `);
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+    console.log(`Server running on ${PORT}`);
 });
